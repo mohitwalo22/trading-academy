7305f28235f2df31610f6d0a937215bf48d432ba
mohitwalo22 <mohit891770@gmail.com>
2026-07-20 12:54:46 +0530

    login.js

--- login.js
+++ login.js
@@ -1,14 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7vAXcLBd7f9rbTeo49p_Gf97CGUZ9_3M",
+  authDomain: "trading-academy-39b6c.firebaseapp.com",
+  projectId: "trading-academy-39b6c",
+  storageBucket: "trading-academy-39b6c.firebasestorage.app",
+  messagingSenderId: "722885461662",
+  appId: "1:722885461662:web:69197ea422cfc2d7ed5d8f"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 const button = document.querySelector("button");
 
 button.addEventListener("click", function () {
+  const email = document.querySelector('input[type="email"]').value;
+  const password = document.querySelector('input[type="password"]').value;
 
-    const email = document.querySelector('input[type="email"]').value;
-    const password = document.querySelector('input[type="password"]').value;
-
-    if(email === "" || password === ""){
-        alert("Please enter your email and password.");
-    }else{
-        alert("Login Successful!");
-    }
-
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert("Login Successful!");
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
 });
